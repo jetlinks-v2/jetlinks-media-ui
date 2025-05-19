a31dbe85eac2b2eca5ef35d286bbcd8ca010d0a6
haodd <1903289197@qq.com>
2025-05-19 15:15:12 +0800

    fix: (国标级联)推送通达弹窗优化发布页面的布局和样式

--- views/Cascade/Publish/index.vue
+++ views/Cascade/Publish/index.vue
@@ -5,34 +5,47 @@
       :title="$t('Publish.index.122692-0')"
       :cancelText="$t('Publish.index.122692-1')"
       :okText="$t('Publish.index.122692-2')"
-      width="900px"
+      width="600px"
       @ok="_vis = false"
       @cancel="_vis = false"
   >
-    <a-row :gutter="20">
-      <a-col :span="8">
-        <p>{{ $t('Publish.index.122692-3') }}{{ successCount }}</p>
-        <a-space>
-          <p>{{ $t('Publish.index.122692-4') }}{{ failCount }}</p>
-          <a
-              v-if="errMessage.length"
-              @click="
-                            downloadJson(
-                                errMessage || '',
-                                data.name + $t('Publish.index.122692-5'),
-                            )
-                        "
-          >{{ $t('Publish.index.122692-6') }}</a
-          >
-        </a-space>
-      </a-col>
-      <a-col :span="8">
-        <p>{{ $t('Publish.index.122692-7') }}{{ data.count }}</p>
-      </a-col>
-      <a-col :span="8">
-        <p>{{ $t('Publish.index.122692-8') }}{{ successCount + failCount }}</p>
-      </a-col>
-    </a-row>
+    <a-card>
+      <a-space align="start" size="large">
+        <AIcon type="CheckCircleFilled" class="status-icon success"></AIcon>
+        <div>
+          <p>成功数</p>
+          <a-space size="large">
+            <span class="count">{{ successCount }}</span>
+            <a-space size="middle">
+              <span>其中通道数量</span>
+              <span>{{ data.count }}</span>
+            </a-space>
+          </a-space>
+        </div>
+      </a-space>
+    </a-card>
+    <div style="margin: 8px;"></div>
+    <a-card>
+      <a-space size="large">
+        <AIcon type="CloseCircleFilled" class="status-icon fail"></AIcon>
+        <div>
+          <p>失败数</p>
+          <a-space size="large">
+            <span class="count">{{ failCount }}</span>
+            <a
+                v-if="errMessage.length"
+                @click="
+                              downloadJson(
+                                  errMessage || '',
+                                  data.name + $t('Publish.index.122692-5'),
+                              )
+                          "
+            >{{ $t('Publish.index.122692-6') }}</a
+            >
+          </a-space>
+        </div>
+      </a-space>
+    </a-card>
     <div v-if="flag">
       <a-textarea :rows="10" v-model:value="errStr"/>
     </div>
@@ -70,7 +83,13 @@ const _vis = computed({
 watch(
     () => _vis.value,
     (val: boolean) => {
-      if (val) publish();
+      if (val) {
+        publish()
+      } else {
+        failCount.value = 0;
+        successCount.value = 0;
+        errMessage.value = [];
+      }
     },
 );
 
@@ -106,3 +125,18 @@ const publish = () => {
   };
 };
 </script>
+<style lang="less" scoped>
+.status-icon {
+  font-size: 80px;
+  &.success {
+    color: var(--ant-success-color);
+  }
+  &.fail {
+    color: var(--ant-error-color);
+  }
+}
+.count {
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
